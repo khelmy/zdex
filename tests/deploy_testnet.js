e667c6b978ea50dbd645ef849c639bc01229bc5d
khelmy <khelmy@andrew.cmu.edu>
2019-02-25 12:55:55 -0500

    Added deploy_testnet

--- tests/deploy_testnet.js
+++ tests/deploy_testnet.js
@@ -8,15 +8,14 @@ const fs = require('fs')
 const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
 
 // These are set by the core protocol, and may vary per-chain.
-// These numbers are JUST AN EXAMPLE. They will NOT WORK on the developer testnet
-// or mainnet.
 // For more information: https://apidocs.zilliqa.com/?shell#getnetworkid
 const CHAIN_ID = 333;
 const MSG_VERSION = 1;
 const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
 
 // Populate the wallet with an account
-const privkey = '3375F915F3F9AE35E6B301B7670F53AD1A5BE15D8221EC7FD5E503F21D3450C8';
+// NOTE: if running on own machine, need to create a file "privkey.txt" with your private key
+const privkey = fs.readFileSync('./privkey_testnet.txt', 'utf8');
 
 zilliqa.wallet.addByPrivateKey(
   privkey
@@ -26,9 +25,57 @@ const address = CP.getAddressFromPrivateKey(privkey);
 console.log("Your account address is:");
 console.log(`0x${address}`);
 
-async function testBlockchain() {
-  try {
+const zdex_code = fs.readFileSync('../contracts/ZDExchange.scilla', 'utf8');
 
+const zdex_init = [
+  // this parameter is mandatory for all init arrays
+  {
+    vname: "_scilla_version",
+    type: "Uint32",
+    value: "1"
+  }
+];
+
+/*
+(owner : ByStr20,
+ total_tokens : Uint128,
+ name : String,
+ symbol : String)
+*/
+
+const token_code = fs.readFileSync('../contracts/FungibleToken.scilla', 'utf8');
+const token_init = [
+  // this parameter is mandatory for all init arrays
+  {
+    vname: "_scilla_version",
+    type: "Uint32",
+    value: "1"
+  },
+  {
+    vname: "owner",
+    type: "ByStr20",
+    value: `0x${address}`
+  },
+  {
+    vname: "total_tokens",
+    type: "Uint128",
+    value: "1000000000000" //10^12 (like zil)
+  },
+  {
+    vname: "name",
+    type: "String",
+    value: "Zilliqa DEX"
+  },
+  {
+    vname: "symbol",
+    type: "String",
+    value: "ZDEX"
+  }
+];
+
+// Verbose deploy function
+async function deploy_v(code, init) {
+  try {
     // Get Balance
     const balance = await zilliqa.blockchain.getBalance(address);
     // Get Minimum Gas Price from blockchain
@@ -36,51 +83,26 @@ async function testBlockchain() {
     console.log(`Your account balance is:`);
     console.log(balance.result)
     console.log(`Current Minimum Gas Price: ${minGasPrice.result}`);
-    const myGasPrice = units.toQa('1000', units.Units.Li); // Gas Price that will be used by all transactions
+    const myGasPrice = units.toQa('15000', units.Units.Li); // Gas Price that will be used by all transactions
     console.log(`My Gas Price ${myGasPrice.toString()}`)
     console.log('Sufficient Gas Price?');
     console.log(myGasPrice.gte(new BN(minGasPrice.result))); // Checks if your gas price is less than the minimum gas price
 
-    /*
-    // Send a transaction to the network
-    const tx = await zilliqa.blockchain.createTransaction(
-      zilliqa.transactions.new({
-        version: VERSION,
-        toAddr: "573EC96638C8BB1C386394602E1460634F02ADDA",
-        amount: new BN(units.toQa("1", units.Units.Zil)), // Sending an amount in Zil (1) and converting the amount to Qa
-        gasPrice: myGasPrice, // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
-        gasLimit: Long.fromNumber(1)
-      })
-    );
-
-    console.log(`The transaction status is:`);
-    console.log(tx.receipt);
-    */
-
     // Deploy a contract
-    var zdex_code = ``;
-    fs.readFile('../contracts/ZDExchange.scilla', 'utf8', function(err, contents) {
-      zdex_code = contents;
-    });
-
-    const init = [
-      // this parameter is mandatory for all init arrays
-      {
-        vname: "_scilla_version",
-        type: "Uint32",
-        value: "1"
-      }
-    ];
-
     // Instance of class Contract
-    const contract = zilliqa.contracts.new(zdex_code, init);
+
+    const contract = zilliqa.contracts.new(code, init);
+
 
     // Deploy the contract
-    const [deployTx, zdex] = await contract.deploy({
-      version: VERSION,
-      gasPrice: myGasPrice,
-      gasLimit: Long.fromNumber(10000)
-    });
+    const [deployTx, ctr] = await contract.deploy({
+        version: VERSION,
+        gasPrice: myGasPrice,
+        gasLimit: Long.fromNumber(10000)
+      },
+      3330,
+      100
+    );
 
     // Introspect the state of the underlying transaction
     console.log(`Deployment Transaction ID: ${deployTx.id}`);
@@ -89,34 +111,22 @@ async function testBlockchain() {
 
     // Get the deployed contract address
     console.log("The contract address is:");
-    console.log(zdex.address);
-    /*
-    (const callTx = await zdex.call(
-      "setzdex",
-      [
-        {
-          vname: "msg",
-          type: "String",
-          value: "zdex World"
-        }
-      ],
-      {
-        // amount, gasPrice and gasLimit must be explicitly provided
-        version: VERSION,
-        amount: new BN(0),
-        gasPrice: myGasPrice,
-        gasLimit: Long.fromNumber(8000),
-      }
-    );
-    console.log(callTx);
-    */
-    //Get the contract state
-    const state = await zdex.getState();
+    console.log(ctr.address);
+
+    const state = await ctr.getState();
     console.log("The state of the contract is:");
     console.log(state);
+    return ctr.address;
   } catch (err) {
     console.log(err);
   }
 }
 
-testBlockchain();
+async function main() {
+  var z_deploy = await deploy_v(zdex_code, zdex_init);
+  console.log(z_deploy);
+  var t_deploy = await deploy_v(token_code, token_init);
+  console.log(t_deploy);
+}
+
+main();
