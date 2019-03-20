edd4d28f61c999d96c27337e565579a9ae6aa783
khelmy <khelmy@andrew.cmu.edu>
2019-03-20 14:20:26 -0400

    Stringify JSON objs

--- tests/common.js
+++ tests/common.js
@@ -3,11 +3,11 @@ const { BN, Long, bytes, units } = require('@zilliqa-js/util');
 const { Zilliqa } = require('@zilliqa-js/zilliqa');
 const CP = require('@zilliqa-js/crypto');
 
-// Bundle and delploy contract call
+// Bundle and deploy contract call
 async function bundle_tx(zilliqa, args, data) {
   try {
     let msg = Object.assign({}, args);
-    msg.data = data;
+    msg.data = JSON.stringify(data);
     let tx = zilliqa.transactions.new(msg);
     let tx_call = await zilliqa.blockchain.createTransaction(tx);
     return tx_call;
