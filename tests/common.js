11b88190135b7b03f96a888f1ea0cdde9bff32dc
khelmy <khelmy@andrew.cmu.edu>
2019-03-20 15:35:39 -0400

    Add priority param

--- tests/common.js
+++ tests/common.js
@@ -8,7 +8,9 @@ async function bundle_tx(zilliqa, args, data) {
   try {
     let msg = Object.assign({}, args);
     msg.data = JSON.stringify(data);
+    msg.priority = false;
     let tx = zilliqa.transactions.new(msg);
+    console.log(tx);
     let tx_call = await zilliqa.blockchain.createTransaction(tx);
     return tx_call;
   } catch (err) {
